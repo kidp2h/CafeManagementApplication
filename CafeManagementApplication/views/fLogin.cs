2a1e3753341237eef8f2b8a20f9b062e66fbfecb
PhuThinh <phuthinh5322@gmail.com>
2021-10-06 22:31:42 +0700

    aaaa

--- CafeManagementApplication/views/fLogin.cs
+++ CafeManagementApplication/views/fLogin.cs
@@ -34,7 +34,6 @@ namespace CafeManagementApplication
         private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
-            if (MessageBox.Show("hellooooooooooooooooo?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
             {
                 e.Cancel = true;
             }
