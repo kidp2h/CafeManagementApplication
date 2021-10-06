4878902cac1287ac7e0307c70a0feb9ac783050d
PhuThinh <phuthinh5322@gmail.com>
2021-10-06 15:27:56 +0700

    test

--- CafeManagementApplication/views/fCafeManager.Designer.cs
+++ CafeManagementApplication/views/fCafeManager.Designer.cs
@@ -29,15 +29,37 @@ namespace CafeManagementApplication.views
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // fCafeManagement
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // fCafeManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1134, 668);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "fCafeManagement";
+            this.Name = "fCafeManager";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Phần mềm quản lý quán cafe";
             this.ResumeLayout(false);
@@ -45,5 +67,8 @@ namespace CafeManagementApplication.views
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
