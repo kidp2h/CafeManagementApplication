b81754b58f72957e513cc1cfc4964a1d01559776
PhuThinh <phuthinh5322@gmail.com>
2021-10-06 15:39:54 +0700

    test 1

--- CafeManagementApplication/views/fCafeManager.Designer.cs
+++ CafeManagementApplication/views/fCafeManager.Designer.cs
@@ -31,6 +31,7 @@ namespace CafeManagementApplication.views
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // button1
@@ -50,12 +51,21 @@ namespace CafeManagementApplication.views
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(75, 140);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 2;
             // 
             // fCafeManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1134, 668);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -70,5 +80,6 @@ namespace CafeManagementApplication.views
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
--- CafeManagementApplication/views/fCafeManager.cs
+++ CafeManagementApplication/views/fCafeManager.cs
@@ -15,6 +15,11 @@ namespace CafeManagementApplication.views
         public fCafeManager()
         {
             InitializeComponent();
-        }   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
     }
 }
