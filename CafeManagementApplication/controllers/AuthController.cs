ba04983d16289f68da27c4d071dd680d1c4bfae5
KidP2H <40386174+kidp2h@users.noreply.github.com>
2021-10-06 15:16:08 +0700

    handle error conflict

--- CafeManagementApplication/controllers/AuthController.cs
+++ CafeManagementApplication/controllers/AuthController.cs
@@ -1,30 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using CafeManagementApplication.views;
 using CafeManagementApplication.models;
+using System.Windows.Forms;
+
 namespace CafeManagementApplication.controllers
 {
-    class AuthController : fLogin
+    class AuthController
     {
         private fLogin _view;
-
+        private readonly User UserModel;
         public AuthController(fLogin view)
         {
             _view = view;
+            UserModel = new User();
         }
 
         public void handleLogin()
         {
-            fCafeManagement f = new fCafeManagement();
-            this._view.Hide();
-            f.ShowDialog();
-            this._view.Show();
+            fCafeManager f = new fCafeManager();
+            string username = this._view.inputUsernameText;
+            string password = this._view.inputPasswordText;
+            bool result = this.UserModel.checkAccount(username, password);
+            if (result)
+            {
+                MessageBox.Show("Dang nhap thanh cong");
+                this._view.Hide();
+                f.ShowDialog();
+                this._view.Show();
+            }
+            else
+            {
+                MessageBox.Show("Dang nhap that bai");
+            }
         }
-        
     }
 }
