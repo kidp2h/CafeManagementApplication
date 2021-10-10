c7d0002dd174d6c1ffa8bf27445ead56f190a0fe
KidP2H <40386174+kidp2h@users.noreply.github.com>
2021-10-10 10:20:02 +0700

    add method get list user

---
+++ CafeManagementApplication/helpers/SaveUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CafeManagementApplication.models;
+
+namespace CafeManagementApplication.helpers
+{
+    class SaveUser
+    {
+        User user;
+    }
+}
