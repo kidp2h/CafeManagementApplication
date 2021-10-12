43767015b4aa4cd29147d41c005b2443d54351c1
LuongThinh123 <luongchithinh123@gmail.com>
2021-10-12 21:07:38 +0700

    Load list table

--- CafeManagementApplication/views/uscManager_Tables.cs
+++ CafeManagementApplication/views/uscManager_Tables.cs
@@ -98,6 +98,7 @@ namespace CafeManagementApplication.views
         {
             Table table = ManagerController.Instance.NewData("Table", this);
             ListViewItem tableLvItem = new ListViewItem(table.TableName);
+            tableLvItem.Tag = table.Id;
             tableLvItem.SubItems.Add(table.Status == sTable.EMPTY ? "Bàn trống" : "Có người"); 
             listViewTableInfor.Items.Add(tableLvItem);
             ManagerController.Instance.AddData("Table", table, this);
